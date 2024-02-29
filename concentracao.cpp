ae3c621770d5a2d56e872a72e44f70ca59264021
binww011 <154006418+binww011@users.noreply.github.com>
2024-02-29 19:30:05 -0300

    Jogo da Concentração - FUP

---
+++ concentracao.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int in, fi;
+    cin >> in >> fi;
+    for (int i = in; i <= fi; i++) {
+        cout << i << " " << fi-i+1 << endl;
+    }
+    return 0;    
+}
